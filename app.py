16f3031302c2d64e74ff743f4b2dbb0a2d7c745b
Sumeet Chandankar <74411928+SHIROLAN@users.noreply.github.com>
2026-06-25 00:35:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.huggingface import HuggingFaceEmbeddings
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_groq import ChatGroq
